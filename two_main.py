4b2a2bea5a6c6ea6a84e1ad08d3f5f1e84aa2c30
Neto test <liza.pryaxina@mail.ru>
2024-09-08 21:01:57 +0300

    New_2 file

--- two_main.py
+++ two_main.py
@@ -136,23 +136,162 @@ class Reviewer(Mentor):
         res = f'Имя: {self.name}\nФамилия: {self.surname}'
         return res
 
-some_reviewer = Reviewer(name='Some', surname='Buddy')
+# some_reviewer = Reviewer(name='Some', surname='Buddy')
+#
+# some_lecturer = Lecturer(name='Some', surname='Buddy')
+# some_lecturer.courses_attached += ['Python']
+#
+# some_student = Student(name='Ruoy', surname='Eman')
+# some_student.courses_in_progress += ['Python', 'Git']
+# some_student.finished_courses += ['Введение в программирование']
+#
+# some_student.rate_hw(some_lecturer, 'Python', 9.9)
+# some_student.rate_hw(some_lecturer, 'Python', 9.9)
+# some_student.rate_hw(some_lecturer, 'Python', 9.9)
+#
+# some_reviewer.rate_hw(some_student, 'Python', 9.9)
+# some_reviewer.rate_hw(some_student, 'Python', 9.9)
+# some_reviewer.rate_hw(some_student, 'Python', 9.9)
+#
+# print(some_reviewer)
+# print(some_lecturer)
+# print(some_student)
 
-some_lecturer = Lecturer(name='Some', surname='Buddy')
-some_lecturer.courses_attached += ['Python']
+# # Создаем лекторов и закрепляем их за курсом
+best_lecturer_1 = Lecturer('Ivan', 'Ivanov')
+best_lecturer_1.courses_attached += ['Python']
 
-some_student = Student(name='Ruoy', surname='Eman')
-some_student.courses_in_progress += ['Python', 'Git']
-some_student.finished_courses += ['Введение в программирование']
+best_lecturer_2 = Lecturer('Petr', 'Petrov')
+best_lecturer_2.courses_attached += ['Java']
 
-some_student.rate_hw(some_lecturer, 'Python', 9.9)
-some_student.rate_hw(some_lecturer, 'Python', 9.9)
-some_student.rate_hw(some_lecturer, 'Python', 9.9)
+best_lecturer_3 = Lecturer('Semen', 'Zarev')
+best_lecturer_3.courses_attached += ['Python']
 
-some_reviewer.rate_hw(some_student, 'Python', 9.9)
-some_reviewer.rate_hw(some_student, 'Python', 9.9)
-some_reviewer.rate_hw(some_student, 'Python', 9.9)
+# # Создаем проверяющих и закрепляем их за курсом
+cool_reviewer_1 = Reviewer('Some', 'Buddy')
+cool_reviewer_1.courses_attached += ['Python']
+cool_reviewer_1.courses_attached += ['Java']
 
-print(some_reviewer)
-print(some_lecturer)
-print(some_student)
+cool_reviewer_2 = Reviewer('Ostap', 'Bender')
+cool_reviewer_2.courses_attached += ['Python']
+cool_reviewer_2.courses_attached += ['Java']
+
+# # Создаем студентов и определяем для них изучаемые и завершенные курсы
+student_1 = Student('Denis', 'Sviridov')
+student_1.courses_in_progress += ['Python']
+student_1.finished_courses += ['Введение в программирование']
+
+student_2 = Student('Roman', 'Malikov')
+student_2.courses_in_progress += ['Java']
+student_2.finished_courses += ['Введение в программирование']
+
+student_3 = Student('Sidor', 'Petrov')
+student_3.courses_in_progress += ['Python']
+student_3.finished_courses += ['Введение в программирование']
+
+# # Выставляем оценки лекторам за лекции
+student_1.rate_hw(best_lecturer_1, 'Python', 10)
+student_1.rate_hw(best_lecturer_1, 'Python', 9)
+student_1.rate_hw(best_lecturer_1, 'Python', 7)
+
+student_1.rate_hw(best_lecturer_2, 'Python', 5)
+student_1.rate_hw(best_lecturer_2, 'Python', 7)
+student_1.rate_hw(best_lecturer_2, 'Python', 8)
+
+student_1.rate_hw(best_lecturer_1, 'Python', 7)
+student_1.rate_hw(best_lecturer_1, 'Python', 8)
+student_1.rate_hw(best_lecturer_1, 'Python', 5)
+
+student_2.rate_hw(best_lecturer_2, 'Java', 5)
+student_2.rate_hw(best_lecturer_2, 'Java', 8)
+student_2.rate_hw(best_lecturer_2, 'Java', 9)
+
+student_3.rate_hw(best_lecturer_3, 'Python', 4)
+student_3.rate_hw(best_lecturer_3, 'Python', 6)
+student_3.rate_hw(best_lecturer_3, 'Python', 8)
+
+# # Выставляем оценки студентам за домашние задания
+cool_reviewer_1.rate_hw(student_1, 'Python', 8)
+cool_reviewer_1.rate_hw(student_1, 'Python', 9)
+cool_reviewer_1.rate_hw(student_1, 'Python', 6)
+
+cool_reviewer_2.rate_hw(student_2, 'Java', 8)
+cool_reviewer_2.rate_hw(student_2, 'Java', 7)
+cool_reviewer_2.rate_hw(student_2, 'Java', 9)
+
+cool_reviewer_2.rate_hw(student_3, 'Python', 8)
+cool_reviewer_2.rate_hw(student_3, 'Python', 7)
+cool_reviewer_2.rate_hw(student_3, 'Python', 10)
+cool_reviewer_2.rate_hw(student_3, 'Python', 8)
+cool_reviewer_2.rate_hw(student_3, 'Python', 7)
+cool_reviewer_2.rate_hw(student_3, 'Python', 9)
+
+# # Выводим характеристики созданных и оцененых студентов в требуемом виде
+print(f'Перечень лекторов:\n\n{best_lecturer_1}\n\n{best_lecturer_2}\n\n{best_lecturer_3}')
+print()
+print()
+
+# # Выводим характеристики созданных и оцененых лекторов в требуемом виде
+print(f'Перечень лекторов:\n\n{best_lecturer_1}\n\n{best_lecturer_2}\n\n{best_lecturer_3}')
+print()
+print()
+
+# # Выводим результат сравнения студентов по средним оценкам за домашние задания
+print(f'Результат сравнения студентов (по средним оценкам за ДЗ): '
+      f'{student_1.name} {student_1.surname} < {student_2.name} {student_2.surname} = {student_1 > student_2}')
+print()
+
+# # Выводим результат сравнения лекторов по средним оценкам за лекции
+print(f'Результат сравнения лекторов (по средним оценкам за лекции): '
+      f'{best_lecturer_1.name} {best_lecturer_1.surname} < {best_lecturer_2.name} {best_lecturer_2.surname} = {best_lecturer_1 > best_lecturer_2}')
+print()
+
+# # Создаем список студентов
+student_list = [student_1, student_2, student_3]
+
+# # Создаем список лекторов
+lecturer_list = [best_lecturer_1, best_lecturer_2, best_lecturer_3]
+
+
+# # Создаем функцию для подсчета средней оценки за домашние задания
+# # по всем студентам в рамках конкретного курса
+# # в качестве аргументов принимает список студентов и название курса
+
+def student_rating(student_list, course_name):
+#     """Функция для подсчета средней оценки за домашние задания
+#     по всем студентам в рамках конкретного курса
+#     в качестве аргументов принимает список студентов и название курса"""
+
+    sum_all = 0
+    count_all = 0
+    for stud in student_list:
+       if stud.courses_in_progress == [course_name]:
+            sum_all += stud.average_rating
+            count_all += 1
+    average_for_all = sum_all / count_all
+    return average_for_all
+
+
+# # Создаем функцию для подсчета средней оценки за лекции всех лекторов в рамках курса
+# # в качестве аргумента принимает список лекторов и название курса
+
+def lecturer_rating(lecturer_list, course_name):
+#     """Функция для подсчета средней оценки за лекции всех лекторов в рамках курса
+#      в качестве аргумента принимает список лекторов и название курса"""
+
+    sum_all = 0
+    count_all = 0
+    for lect in lecturer_list:
+        if lect.courses_attached == [course_name]:
+            sum_all += lect.average_rating
+            count_all += 1
+    average_for_all = sum_all / count_all
+    return average_for_all
+
+
+# # Выводим результат подсчета средней оценки по всем студентам для данного курса
+print(f"Средняя оценка для всех студентов по курсу {'Python'}: {student_rating(student_list, 'Python')}")
+print()
+
+# # Выводим результат подсчета средней оценки по всем лекорам для данного курса
+print(f"Средняя оценка для всех лекторов по курсу {'Python'}: {lecturer_rating(lecturer_list, 'Python')}")
